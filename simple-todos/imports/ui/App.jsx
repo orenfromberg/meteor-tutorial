5fe2644892bec5101ee0023cbaa69932263ca0ba
Oren <orenfromberg@gmail.com>
2016-11-14 00:39:17 -0500

    temporary ui state

--- simple-todos/imports/ui/App.jsx
+++ simple-todos/imports/ui/App.jsx
@@ -7,6 +7,14 @@ import Task from './Task.jsx';
 
 //App component represents the whole App
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
     handleSubmit(event) {
         event.preventDefault();
 
@@ -21,6 +29,12 @@ class App extends Component {
         this.textInput.value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     getTasks() {
         return [
             { _id: 1, text: 'This is task 1' },
@@ -30,7 +44,11 @@ class App extends Component {
     }
 
     renderTasks() {
-        return this.props.tasks.map(task => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map(task => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -39,7 +57,16 @@ class App extends Component {
         return (
             <div className='container'>
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+
+                    <label className="hide-completed">
+                        <input type="checkbox"
+                        readOnly
+                        checked={this.state.hideCompleted}
+                        onClick={this.toggleHideCompleted.bind(this)}/>
+                        Hide Completed Tasks
+                    </label>
+
                     <form className='new-task' onSubmit={this.handleSubmit.bind(this)} >
                         <input type='text'
                             ref={input => this.textInput = input}
@@ -57,11 +84,13 @@ class App extends Component {
 
 App.propTypes = {
     tasks: PropTypes.array.isRequired,
+    incompleteCount: PropTypes.number.isRequired,
 }
 
 export default createContainer(() => {
     return {
         // tasks: Tasks.find({}).fetch(),
         tasks: Tasks.find({}, { sort: { createdAt: -1}}).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true }}).count(),
     };
 }, App);
